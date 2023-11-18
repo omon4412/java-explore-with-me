508bc682df125e315c9a5d202d7a1bbba167642a
Vetrov Sergey <vetrov241201@yandex.ru>
2023-11-18 22:30:25 +0300

    Сделал сервис статистики + docker

---
+++ statistic-service/client/src/main/java/ru/practicum/statiticservice/client/StatisticClient.java
@@ -0,0 +1,53 @@
+package ru.practicum.statiticservice.client;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.DefaultUriBuilderFactory;
+import ru.practicum.statisticservice.dto.EndpointHitDto;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class StatisticClient extends BaseClient {
+    @Value("${statistic-server.dateFormat}")
+    private static String format;
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern(format);
+
+    public StatisticClient(RestTemplate rest) {
+        super(rest);
+    }
+
+    @Autowired
+    public StatisticClient(@Value("${statistic-server.url}") String serverUrl, RestTemplateBuilder builder) {
+        super(
+                builder
+                        .uriTemplateHandler(new DefaultUriBuilderFactory(serverUrl))
+                        .requestFactory(HttpComponentsClientHttpRequestFactory::new)
+                        .build()
+        );
+    }
+
+    public ResponseEntity<Object> addStatistic(EndpointHitDto endpointHitDto) {
+        return post("/hit", endpointHitDto);
+    }
+
+    public ResponseEntity<Object> getStatistic(LocalDateTime start, LocalDateTime end,
+                                               List<String> uris,
+                                               Boolean unique) {
+        Map<String, Object> parameters = Map.of(
+                "start", start.format(formatter),
+                "end", end.format(formatter),
+                "uris", uris,
+                "unique", unique
+        );
+        return get("/stats?start={start}&end={end}&uris={uris}&unique={unique}", parameters);
+    }
+}
